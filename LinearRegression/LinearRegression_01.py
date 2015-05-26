56531f8251139fd8e96597f35802cea5ee3e94c0
Eilliar <lord.eilliar@hotmail.com>
2015-05-25 21:01:13 -0300

    Create Machine Learing Repository =)

---
+++ LinearRegression/LinearRegression_01.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Linear Regression 01
+
+Author: Bruno Godoi Eilliar
+Date: May 19, 2015
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+debug = True
+x = np.array([0., 1, 2, 3])
+y = np.array([4., 7, 7, 8])
+m = float(len(x)) # make sure to work with float number always
+alpha = .1
+
+plt.figure(1)
+plt.plot(x, y, 'bo')
+plt.axis([-1, 5, 0, 10])
+plt.draw()
+plt.show()
+
+theta0 = 2.
+theta1 = 2.
+
+
+# Compute h_theta
+h_theta = theta0 + theta1*x
+
+# Compute Cost Function
+a = (h_theta - y)**2
+J = (1/(2*m))*np.sum(a)
+if debug: print "Cost: ", J
+
+for i in range(1, 1000):
+    theta0_new = theta0 -alpha*(1/m)*np.sum((h_theta - y))
+    theta1_new = theta1 - alpha*(1/m)*np.sum(np.dot((h_theta - y), x))
+    if debug:
+        print "(Theta0, Theta1): ", theta0_new, theta1_new
+    theta0 = np.copy(theta0_new)
+    theta1 = np.copy(theta1_new)
+    h_theta = theta0 + theta1*x
+
+plt.figure(1)
+plt.plot(x, h_theta, 'r--')
+plt.draw()
+plt.show()
