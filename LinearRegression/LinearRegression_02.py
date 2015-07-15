bec12dcead67b1eb9125ea9551324f70b272ef01
Eilliar <lord.eilliar@hotmail.com>
2015-07-15 13:31:16 -0300

    Implement Gradient Descent loop in LinearRegression02.

--- LinearRegression/LinearRegression_02.py
+++ LinearRegression/LinearRegression_02.py
@@ -31,3 +31,41 @@ plt.close("all")
 plt.figure(1)
 plt.plot(x, y, 'ro')
 plt.show()
+
+"""
+Fill In with your code bellow
+"""
+# Create a new figure to plot the data and the hypothesis
+plt.figure(2)
+plt.ion()
+
+# Define alpha
+alpha = 0.1
+# Size of data set
+m = float(len(x))
+# Define Hypothesis
+theta0 = 0.
+theta1 = 0.
+# Compute h_theta
+h_theta = theta0 + theta1*x
+# Compute the Cost Function
+a = (h_theta - y)**2
+J = (1/(2*m))*np.sum(a)
+
+
+for i in range(1, 100):
+    theta0_new = theta0 -alpha*(1/m)*np.sum((h_theta - y))
+    theta1_new = theta1 - alpha*(1/m)*np.sum(np.dot((h_theta - y), x))
+    theta0 = np.copy(theta0_new)
+    theta1 = np.copy(theta1_new)
+    h_theta = theta0 + theta1*x
+
+    #Show the fitting process
+    plt.figure(2)
+    plt.clf()
+    plt.plot(x, y, 'bo')
+    plt.axis([0, 1, 2.5, 5])
+    plt.plot(x, h_theta, 'r--')
+    plt.draw()
+    plt.show()
+    plt.pause(0.01)
